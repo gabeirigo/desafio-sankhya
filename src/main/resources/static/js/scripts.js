7bfed6d43b544ca37bfd9421d6e36b554e1be617
gabeirigo <gabriel.beirigoweb@gmail.com>
2018-03-28 13:25:47 -0300

    Atualização de arquivo

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,5 +1,4 @@
 $(function() {
-
 	$('.money').mask('000.000.000.000.000,00', {reverse: true});
 
 	// FORMATAR MOEDA
@@ -13,7 +12,7 @@ $(function() {
 	function formatDate(date) {
 		var d = new Date(date),
 		month = '' + (d.getMonth() + 1),
-		day = '' + d.getDate(),
+		day = '' + (d.getDate() + 1),
 		year = d.getFullYear();
 
 		if (month.length < 2) month = '0' + month;
@@ -27,7 +26,4 @@ $(function() {
 		dataFormatada = formatDate(recebeData);
 		$(this).find('.date').text(dataFormatada);
 	})
-
-	
-
 });
