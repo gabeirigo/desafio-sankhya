423c98a87795bd56a21f4bbba1ed2a83d22edf5c
DESKTOP-HLO19LE\gabri <gabriel.beirigoweb@gmail.com>
2018-03-28 13:24:30 -0300

    Atualização de arquivo

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -13,7 +13,7 @@ $(function() {
 	function formatDate(date) {
 		var d = new Date(date),
 		month = '' + (d.getMonth() + 1),
-		day = '' + d.getDate(),
+		day = '' + (d.getDate() + 1),
 		year = d.getFullYear();
 
 		if (month.length < 2) month = '0' + month;
