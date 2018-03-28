fb1ec97f6f4e57a84d83129100da7345611e371b
DESKTOP-HLO19LE\gabri <gabriel.beirigoweb@gmail.com>
2018-03-28 17:23:20 -0300

    Fix bug - Formato de data

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,4 +1,5 @@
 $(function() {
+
 	$('.money').mask('000.000.000.000.000,00', {reverse: true});
 
 	// FORMATAR MOEDA
@@ -9,21 +10,26 @@ $(function() {
 	})
 
 	// FUNCÃO PARA FORMATAR DATA
-	function formatDate(date) {
-		var d = new Date(date),
-		month = '' + (d.getMonth() + 1),
-		day = '' + (d.getDate() + 1),
-		year = d.getFullYear();
+	function formatDate(dateObject) {
+	    var d = new Date(dateObject);
+	    var day = d.getDate();
+	    var month = d.getMonth() + 1;
+	    var year = d.getFullYear();
+	    if (day < 10) {
+	        day = "0" + day;
+	    }
+	    if (month < 10) {
+	        month = "0" + month;
+	    }
+	    var date = day + "/" + month + "/" + year;
 
-		if (month.length < 2) month = '0' + month;
-		if (day.length < 2) day = '0' + day;
-
-		return [day, month, year].join('/');
-	}
+	    return date;
+	};
 	
 	$('tr').each(function() {
 		var recebeData = $(this).find('.date').text();
 		dataFormatada = formatDate(recebeData);
 		$(this).find('.date').text(dataFormatada);
 	})
+
 });
